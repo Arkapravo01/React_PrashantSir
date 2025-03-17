08ca8d5eb3bb2bccfde9d993ccc2bbb011f1342e
Arkapravo01 <arkapravochakrabarti188@gmail.com>
2025-03-17 17:12:16 +0530

    Children

--- 18-fragments/src/App.jsx
+++ 18-fragments/src/App.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import FoodItems from "./Components/FoodItems";
 import ErrorMessage from "./Components/ErrorMessage";
-import './App.css'
+import "./App.css";
+import Container from "./Components/Container";
 
 function App() {
-  let foodItems = [
-    
-  ];
+  let foodItems = ["Dal", "Sabji", "Roti", "Salad", "Banana", "Pizza"];
   //Logical Operators Conditional Rendering
   // let emptymessage = foodItems.length == 0 ? <h3>I am still hungry</h3> : null;
   return (
     <>
-      <h1>Healthy Foods:</h1>
-      <FoodItems foodItems={foodItems} />
-      <ErrorMessage foodItems={foodItems} />
+      <Container>
+        <h1>Healthy Foods:</h1>
+        <FoodItems foodItems={foodItems} />
+        <ErrorMessage foodItems={foodItems} />
+      </Container>
+      <Container>
+        <p>Above is the list of healthy foods that are good for our health and well-being</p>
+      </Container>
     </>
   );
 }
